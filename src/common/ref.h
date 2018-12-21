a42b96f42e80b1b4cd11a3ae8c33b4b1ad00672d
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-12-21 12:23:01 +0000

    Move q2 render interface to common code

--- src/common/ref.h
+++ src/common/ref.h
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "../qcommon/qcommon.h"
+//#include "../qcommon/qcommon.h"
 
 #define	MAX_DLIGHTS		32
 #define	MAX_ENTITIES	128
