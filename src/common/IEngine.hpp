b67fa9b0593e66873fe47bac2769779b3ff2ceda
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-01-06 00:17:24 +0000

    Add IEngine interface

---
+++ src/common/IEngine.hpp
@@ -0,0 +1,30 @@
+/// @file
+
+#pragma once
+
+#ifdef _WIN32
+#	include <windows.h>
+#endif
+
+struct IEngine
+{
+	struct InitProps
+	{
+		struct PlatformProps
+		{
+#ifdef _WIN32
+			HINSTANCE hInstance{};
+#endif
+		};
+		
+		PlatformProps PlatformSpecific{};
+	};
+	
+	///
+	virtual bool Init(const InitProps &apInitProps) = 0;
+	
+	///
+	virtual void Frame() = 0;
+};
+
+using fnGetEngine = IEngine *(*)();
