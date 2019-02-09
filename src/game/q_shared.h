c8f9315c075317c862ccd51c43f7dc3e89260d4e
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-02-09 13:25:19 +0000

    Update game module q_shared header (define qboolean as int for cpp)

--- src/game/q_shared.h
+++ src/game/q_shared.h
@@ -52,8 +52,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 typedef unsigned char 		byte;
-typedef enum {false, true}	qboolean;
 
+#ifdef __cplusplus
+typedef int qboolean;
+#else
+typedef enum {false, true}	qboolean;
+#endif
 
 #ifndef NULL
 #define NULL ((void *)0)
