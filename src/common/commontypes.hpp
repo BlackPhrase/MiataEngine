dcf8ad76f95cb1df4181421e3de915edb04e0b7a
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-26 17:44:16 +0000

    Restore commontypes header

---
+++ src/common/commontypes.hpp
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2017-2019 BlackPhrase
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+/// @brief general definitions
+
+#pragma once
+
+#ifdef _WIN32
+	#define EXPORT [[dllexport]]
+#else
+	#define EXPORT [[visibility("default")]]
+#endif
+
+#define C_EXPORT extern "C" EXPORT
+
+constexpr auto BIT(int anNum){return 1<<(anNum);}
+
+#ifndef BYTE_DEFINED
+	using byte = unsigned char;
+	#define BYTE_DEFINED 1
+#endif
+
+using qboolean = int;
