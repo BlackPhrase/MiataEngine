482cbe27bc234d957f6af393d78a1c855dd20859
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-26 17:41:14 +0000

    Move host functionality decls to a separate header

--- src/engine/quakedef.h
+++ src/engine/quakedef.h
@@ -288,45 +288,7 @@ typedef struct
 
 extern qboolean noclip_anglehack;
 
-
-//
-// host
-//
-extern	quakeparms_t host_parms;
-
-extern	cvar_t		sys_ticrate;
-extern	cvar_t		sys_nostdout;
-extern	cvar_t		developer;
-
-extern	qboolean	host_initialized;		// true if into command execution
-extern	double		host_frametime;
-extern	byte		*host_basepal;
-extern	byte		*host_colormap;
-extern	int			host_framecount;	// incremented every frame, never reset
-extern	double		realtime;			// not bounded in any way, changed at
-										// start of every frame, never reset
-
-void Host_ClearMemory (void);
-void Host_ServerFrame (void);
-void Host_InitCommands (void);
-void Host_Init (quakeparms_t *parms);
-void Host_Shutdown(void);
-void Host_Error (char *error, ...);
-void Host_EndGame (char *message, ...);
-void Host_Frame (float time);
-void Host_Quit_f (void);
-void Host_ClientCommands (char *fmt, ...);
-void Host_ShutdownServer (qboolean crash);
-
-extern qboolean		msg_suppress_1;		// suppresses resolution and cache size console output
-										//  an fullscreen DIB focus gain/loss
-extern int			current_skill;		// skill level for currently loaded level (in case
-										//  the user changes the cvar while the level is
-										//  running, this reflects the level actually in use)
-
-extern qboolean		isDedicated;
-
-extern int			minimum_memory;
+#include "host.h"
 
 //
 // chase
