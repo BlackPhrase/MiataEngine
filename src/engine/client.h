e4fb217e1abf440067ced100353ca732ce3342d9
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-06-01 06:09:01 +0000

    Move usercmd_t struct to common code

--- src/engine/client.h
+++ src/engine/client.h
@@ -19,18 +19,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // client.h
 
+#include "usercmd.h"
+
 typedef struct
 {
-	vec3_t	viewangles;
 
-// intended velocities
-	float	forwardmove;
-	float	sidemove;
-	float	upmove;
-#ifdef QUAKE2
-	byte	lightlevel;
-#endif
-} usercmd_t;
 
 typedef struct
 {
