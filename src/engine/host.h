a1833a47371e4a18dc7a7c87e23fef28df61c508
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-05-30 08:26:51 +0000

    [Engine] More const-correctness fixes (2)

--- src/engine/host.h
+++ src/engine/host.h
@@ -28,11 +28,11 @@ void SV_Frame (float time);
 void Host_InitCommands (void);
 void Host_Init (quakeparms_t *parms);
 void Host_Shutdown(void);
-void Host_Error (char *error, ...);
-void Host_EndGame (char *message, ...);
+void Host_Error (const char *error, ...);
+void Host_EndGame (const char *message, ...);
 void Host_Frame (float time);
 void Host_Quit_f (void);
-void Host_ClientCommands (char *fmt, ...);
+void Host_ClientCommands (const char *fmt, ...);
 void Host_ShutdownServer (qboolean crash);
 void Host_WriteConfiguration (void);
 
